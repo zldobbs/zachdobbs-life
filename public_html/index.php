2e033dcff20f017e76e9bb8b8be8e666573a34a4
Zach Dobbs <zld6fd@mail.missouri.edu>
2017-09-17 16:29:32 -0500

    Update revisions for current model

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- Create github repo for this site -->
 <html>
   <head>
     <title>Zachary Dobbs</title>
@@ -23,62 +22,11 @@
       integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
       crossorigin="anonymous"></script>
 
-    <!-- CSS Sandbox, move to external sheet and organize by categories, cleanup unused classes/ids -->
+    <!-- CSS Sandbox -->
     <style>
       body {
         background-color: #2c332e;
         font-family: 'Arimo', sans-serif;
-      }
-
-      .panel-body {
-        margin-bottom: 15px;
-      }
-
-      .panel-img {
-        /*float: left;*/
-      }
-
-      .panel-img img {
-        width: 360px;
-        height: 225px;
-        box-shadow: 2px 2px 3px #2c332e;
-        transition: all .2s ease-in-out;
-      }
-
-      .panel-img img:hover {
-          transform: scale(1.1);
-      }
-
-      .panel-text {
-        margin-left: 35px;
-        margin-top: 35px;
-      }
-
-      .jumbotron {
-        border-radius: 0;
-        /* I don't think this is working. Change it to sections
-        with container-fluids. use background color */
-      }
-
-      #profile-pic {
-        border: 2px solid #ededed;
-        box-shadow: 2px 2px 2px #000;
-        margin-bottom: 15px;
-      }
-
-      .area {
-        background-color: #427c61;
-        margin-top: 50px;
-        color: #ededed;
-        padding: 20px;
-      }
-
-      #welcome {
-        background-color: #ededed;
-      }
-
-      #welcome h1 {
-        text-shadow: none;
       }
 
       h1 {
@@ -95,6 +43,41 @@
         color: #ededed;
       }
 
+      video {
+        width: 100%;
+        height: auto;
+      }
+
+      img.img-responsive {
+        margin: 0 auto;
+      }
+
+      .panel-body {
+        margin-bottom: 15px;
+      }
+
+      .panel-text {
+        margin-top: 35px;
+      }
+
+      .panel-img img {
+        width: 360px;
+        height: 225px;
+        box-shadow: 2px 2px 3px #2c332e;
+        transition: all .2s ease-in-out;
+      }
+
+      .panel-img img:hover {
+        transform: scale(1.1);
+      }
+
+      .area {
+        background-color: #427c61;
+        margin-top: 50px;
+        color: #ededed;
+        padding: 20px;
+      }
+
       .nav-pills li {
         float: none;
         display: inline-block;
@@ -108,22 +91,47 @@
         background-color: #ededed;
         transition: all .2s ease-in-out;
       }
+
       .nav-pills li a:hover {
         color: #ededed;
         background-color: #2c332e;
+      }
+
+      .btn {
+        color: #2c332e;
+        background-color: #ededed;
+      }
+
+      .btn:hover {
+        color: #ededed;
+        background-color: #2c332e;
+        transition: all .2s ease-in-out;
+      }
+
+      .btn a {
+        color: #2c332e;
+      }
+
+      .btn:hover a {
+        color: #ededed;
       }
 
       .container-fluid {
         text-align: center;
       }
 
-      video {
-        width: 100%;
-        height: auto;
+      #profile-pic {
+        border: 2px solid #ededed;
+        box-shadow: 2px 2px 2px #000;
+        margin-bottom: 15px;
       }
 
-      img.img-responsive {
-        margin: 0 auto;
+      #welcome {
+        background-color: #ededed;
+      }
+
+      #welcome h1 {
+        text-shadow: none;
       }
     </style>
   </head>
@@ -159,11 +167,17 @@
     <!-- Portfolio -->
     <div class="container-fluid area">
       <h1>Portfolio</h1>
-      <!-- Flask -->
+      <p>Some of my favorite projects I have developed in the past few years. Live demos can be accessed by clicking
+        the project's image.</p>
+      <!-- Row 1 -->
       <div class="row">
-
+        <!-- Flask -->
         <div class="col-md-6 col-xs-12">
-          <div class="panel-heading"><h2>Flask Blogger!</h2></div>
+          <div class="panel-heading">
+            <a href="http://www.zachflask.tk">
+              <h2>Flask Blogger!</h2>
+            </a>
+          </div>
           <div class="row panel-body">
             <div class="panel-img">
               <a href="http://www.zachflask.tk">
@@ -171,29 +185,44 @@
               </a>
             </div>
             <div class="panel-text">
-              <p>A blogging application developed using Flask.</p>
+              <p>A sample application developed using the Python microframework Flask. This blogging site
+              was a fun exploration for me to learn about the possibilities of using Flask.</p>
+              <p class="btn"><a href="https://github.com/zldobbs/flask-app">GitHub Source</a></p>
             </div>
           </div>
         </div>
-
-      <!-- Titter -->
+        <!-- Titter -->
         <div class="col-md-6 col-xs-12">
-          <div class="panel-heading"><h2>Titter</h2></div>
+          <div class="panel-heading">
+            <a href="https://zld6fd-sample-app.herokuapp.com">
+              <h2>Rails Application</h2>
+            </a>
+          </div>
           <div class="row panel-body">
             <div class="panel-img">
-              <a href="http://localhost:8080">
+              <a href="https://zld6fd-sample-app.herokuapp.com">
                 <img class="img-responsive" src="img/twitter_screen.png" alt="Titter Website">
               </a>
             </div>
             <div class="panel-text">
-              <p>This is a Twitter Clone website. WIP</p>
+              <p>A twitter clone developed using Ruby on Rails. Based on the fantastic tutorial by
+              <a href="https://www.railstutorial.org"><b>Michael Hartl</b></a>. This project is something I am
+              currently still developing.</p>
+              <p class="btn"><a href="https://github.com/zldobbs/sample_app">GitHub Source</a></p>
             </div>
           </div>
         </div>
+      </div>
 
-      <!-- Kendrick -->
+      <!-- Row 2 -->
+      <div class="row">
+        <!-- Kendrick -->
         <div class="col-md-6 col-xs-12">
-          <div class="panel-heading"><h2>Kendrick Lamar</h2></div>
+          <div class="panel-heading">
+            <a href="/kendrick/">
+              <h2>Kendrick Lamar</h2>
+            </a>
+          </div>
           <div class="row panel-body">
             <div class="panel-img">
               <a href="/kendrick/">
@@ -201,14 +230,20 @@
               </a>
             </div>
             <div class="panel-text">
-              <p>This is a Kendrick Lamar website.</p>
+              <p>A website dedicated to the works of the rap artist Kendrick Lamar. Developed as a final project
+              for a web development course. Features jQuery elements, php form-handling with login authentication,
+              AJAX get requests using jQuery, templating using php include statements, and much more.</p>
+              <p class="btn"><a href="https://github.com/zldobbs/kendrick">GitHub Source</a></p>
             </div>
           </div>
         </div>
-
-      <!-- LIVES -->
+        <!-- LIVES -->
         <div class="col-md-6 col-xs-12">
-          <div class="panel-heading"><h2>LIVES</h2></div>
+          <div class="panel-heading">
+            <a href="http://www.lakesidegoats.dx.am">
+              <h2>LIVES</h2>
+            </a>
+          </div>
           <div class="row panel-body">
             <div class="panel-img">
               <a href="http://www.lakesidegoats.dx.am">
@@ -216,14 +251,25 @@
               </a>
             </div>
             <div class="panel-text">
-              <p>This is a goat website.</p>
+              <p>A web application developed in conjuction with my cousin, a small town goat farmer.
+              I collaborated one summer with him to create this inventory management tool that helped
+              increase his farm's productivity. This project introduced me to database configuration and
+              maintenance, php scripting, and Bootstrap interfaces - as well as how to deliver effective
+              results to a client with no technical background.</p>
+              <p class="btn"><a href="https://github.com/zldobbs/lakeside-goats">GitHub Source</a></p>
             </div>
           </div>
         </div>
-
-      <!-- Cycle Fitness -->
+      </div>
+      <!-- Row 3 -->
+      <div class="row">
+        <!-- Cycle Fitness -->
         <div class="col-md-6 col-md-offset-3 col-xs-12">
-          <div class="panel-heading"><h2>Cycle Fitness</h2></div>
+          <div class="panel-heading">
+            <a href="/cycle/">
+              <h2>Cycle Fitness</h2>
+            </a>
+          </div>
           <div class="row panel-body">
             <div class="panel-img">
               <a href="/cycle/">
@@ -231,14 +277,21 @@
               </a>
             </div>
             <div class="panel-text">
-              <p>This is a Cycle Fitness website.</p>
+              <p>Website developed for an FBLA web design competition during my Junior year of high school.
+              Developed using vanilla HTML, CSS, and jQuery. Won sixth place competing at the Illinois
+              state level.</p>
+              <p class="btn"><a href="https://github.com/zldobbs/cycle-fitness">GitHub Source</a></p>
             </div>
           </div>
         </div>
-
+      </div>
+      <!-- Row 4 -->
+      <div class="row">
         <!-- Snake Game -->
         <div class="col-md-8 col-md-offset-2 col-xs-12">
-          <div class="panel-heading"><h2>Snake</h2></div>
+          <div class="panel-heading">
+            <h2>Snake</h2>
+          </div>
           <div class="row panel-body">
             <video width="560" height="315" controls>
               <source src="img/snake.mp4" type="video/mp4">
@@ -246,18 +299,24 @@
             </video>
           </div>
           <div class="panel-text">
-            <p>A snake game I created in high school using Python</p>
+            <p>Classic snake game created using Pascal. Developed after being introduced to procedural
+            programming my Junior year of high school. Features different game modes of varying difficulty,
+            options to change the snake's appearance, and local leaderboards.</p>
+            <p class="btn"><a href="https://github.com/zldobbs/snake">GitHub Source</a></p>
           </div>
         </div>
-
       </div>
     </div>
 
     <!-- Footer -->
     <div class="container-fluid area">
       <div class="row">
-        <h3><a href="mailto:dobbszach@gmail.com">&copy; Zachary Dobbs 2017</a></h3>
+        <ul class="nav nav-pills">
+          <li><a href="mailto:dobbszach@gmail.com">&copy; Zachary Dobbs 2017</a></li>
+          <li><a href="https://github.com/zldobbs/zachdobbs-life">View this page's GitHub Source</a></li>
+        </ul>
       </div>
     </div>
+    
   </body>
 </html>
